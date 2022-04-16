64e9c309962d27ac9172b3b09476b78550d74f1a
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-04-16 16:25:54 +0200

    first commit

---
+++ qrgenerator.js
@@ -0,0 +1,21 @@
+const QR = require("qrcode");
+const Excel = require("xlsx");
+
+function createQR(filename, data) {
+  try {
+    QR.toFile("images/" + filename + ".png", data, {
+      width: 500,
+    });
+  } catch (e) {
+    console.log("C'è un errore!");
+  }
+}
+
+const ws = Excel.readFile("urls.xlsx").Sheets["Foglio1"];
+const data = Excel.utils.sheet_to_json(ws);
+
+console.log(data);
+
+data.forEach(function (r) {
+  createQR(r.FILENAME, r.URL);
+});
